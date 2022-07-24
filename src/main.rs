3e50df703f16f2ce722d1bf072faf2a90522c031
Matt Henry <hey@matthenry.fyi>
2022-07-24 11:42:39 -0400

    Get url from CLI argument

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
+use clap::{Arg, App};
 mod urls;
 
 fn main() {
-    let url = "http://example.com";
+    let matches = App::new("braüs")
+                        .arg(Arg::with_name("url"))
+                        .help("URL to load")
+                        .get_matches();
+
+    let url = matches.value_of("url").unwrap();
     let (scheme, url) = urls::parse_url(url);
 
     let (host, path) = urls::get_host_and_path(url);
-    println!("{}", host);
-    println!("{}", path);
+    println!("scheme: {}", scheme);
+    println!("host: {}", host);
+    println!("path: {}", path);
 }
 
